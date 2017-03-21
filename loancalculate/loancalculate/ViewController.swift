f7adbea11ba092ecd52a930500795b48f07ec994
上海海洋大学 <shanhaikaiyoudaigaku@shanghaihaiyangdaxuedeimac-4.local>
2017-03-21 20:31:04 +0800

    第一周

--- loancalculate/loancalculate/ViewController.swift
+++ loancalculate/loancalculate/ViewController.swift
@@ -9,7 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var loanAmountField: UITextField!
+    
+    
+    @IBOutlet weak var loanTermField: UITextField!
+    
+    
+    @IBOutlet weak var interestRateField: UITextField!
+    
+    
+    @IBOutlet weak var result: UILabel!
+    
+    
+    @IBAction func simplebutton(_ sender: Any) {
+        let calculate1=simpleCalculate()
+        result.text=calculate1.calculate(loanAmount:Double(loanAmountField.text!)!, years: Int(loanTermField.text!)!, interestRate: Double(interestRateField.text!)!).description
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -23,3 +39,10 @@ class ViewController: UIViewController {
 
 }
 
+class simpleCalculate{
+    func calculate(loanAmount:Double,years:Int,interestRate:Double)->Double{
+        let rate=interestRate/100
+        let interest=loanAmount*rate*Double(years)
+        return loanAmount+interest
+    }
+}
