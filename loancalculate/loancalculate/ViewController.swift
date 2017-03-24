0345fcbaf55939ccb7a6cb763af5c486d8562d43
上海海洋大学 <shanhaikaiyoudaigaku@shanghaihaiyangdaxuedeimac-4.local>
2017-03-24 20:31:41 +0800

    第二周

--- loancalculate/loancalculate/ViewController.swift
+++ loancalculate/loancalculate/ViewController.swift
@@ -21,10 +21,35 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var result: UILabel!
     
-    
+    //实例化
+    @IBAction func compoundbutton(_ sender: Any) {
+        let calculate2=compoundCalculate()
+        result.text=calculate2.calculate(loanAmount:Double(loanAmountField.text!)!, years: Int(loanTermField.text!)!, interestRate: Double(interestRateField.text!)!).money
+    }
     @IBAction func simplebutton(_ sender: Any) {
         let calculate1=simpleCalculate()
-        result.text=calculate1.calculate(loanAmount:Double(loanAmountField.text!)!, years: Int(loanTermField.text!)!, interestRate: Double(interestRateField.text!)!).description
+        guard let loanTermField = loanTermField.text,let loanAmountField = loanAmountField.text,let interestRateField=interestRateField.text
+            else{
+                print("用户没有输入全部数据")
+                return
+        }
+        guard let loanTerm=Int(loanTermField),let loanAmount=Double(loanAmountField),let interestRate=Double(interestRateField)
+            else{
+                print("用户输入格式错误")
+                return
+        }
+        result.text=calculate1.calculate(loanAmount:loanAmount, years: loanTerm, interestRate: interestRate).money
+        //result.text=calculate1.calculate(loanAmount:Double(loanAmountField.text!)!, years: Int(loanTermField.text!)!, interestRate: Double(interestRateField.text!)!).money
+    }
+    
+    
+    
+    //shift+command+0调出官方参考文档
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        //文本框输入的时候，会成为firstrespomder
+        loanAmountField.resignFirstResponder()
+        loanTermField.resignFirstResponder()
+        interestRateField.resignFirstResponder()
     }
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,7 +63,7 @@ class ViewController: UIViewController {
 
 
 }
-
+//简单计算
 class simpleCalculate{
     func calculate(loanAmount:Double,years:Int,interestRate:Double)->Double{
         let rate=interestRate/100
@@ -46,3 +71,27 @@ class simpleCalculate{
         return loanAmount+interest
     }
 }
+//复利计算
+class compoundCalculate {
+    func calculate(loanAmount:Double,years:Int,interestRate:Double)->Double{
+        let rate=interestRate/100
+        let compoundInterest=pow(1.0+rate, Double(years))
+        return compoundInterest*loanAmount
+    }
+}
+//entension 可以扩展功能，扩展对象包括class，enum，struct
+extension Double{
+    var money:String{
+        let formatter=NumberFormatter()
+        formatter.numberStyle = .currency
+        let result:String?
+        result=formatter.string(from: NSNumber(value:self))
+        if result==nil{
+            return"format error"
+        }
+        return result!
+    }
+}
+
+
+
